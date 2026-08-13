7b737d26857d1412764a0b66512bfba333057438
Mr-Ajay-Singh <1rn18cs009.ajay@gmail.com>
2026-08-13 07:15:52 +0530

    fix: change PM2 app name to hobby-backend to prevent overwriting existing Node Server process

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
         {
-            name: 'Node Server',
+            name: 'hobby-backend',
             script: 'server.js',
             instances: 1,
             autorestart: true,
